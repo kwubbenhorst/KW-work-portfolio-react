6d1df1929ecdb8d7e276daed76505476f84ed707
kwubbenhorst <140316693+kwubbenhorst@users.noreply.github.com>
2024-03-07 12:13:33 -0500

    changes to vite.config.js so configuration is set to deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://kwubbenhorst.github.io/KW-work-portfolio-react',
   plugins: [react()],
-  server: {
-    port: 3007,
-    open: true
-  }
 });
